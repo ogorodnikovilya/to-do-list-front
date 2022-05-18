53c6efe06f547b483e3c3c55662343d581b59db7
Ilya <ogorodnikov.fortech@gmail.com>
2022-05-18 16:10:18 +0300

    fixe sort and comment

--- script.js
+++ script.js
@@ -63,74 +63,68 @@ const render = () => {
     list.removeChild(list.firstChild);
   };
 
-  const sortAllTasks = structuredClone(allTasks);
+  const sortAllTasks = allTasks.sort((a, b) => a.isCheck > b.isCheck ? 1 : a.isCheck < b.isCheck ? -1 : 0);
 
-  sortAllTasks.sort((a, b) => a.isCheck > b.isCheck ? 1 : a.isCheck < b.isCheck ? -1 : 0);
+  sortAllTasks.forEach(el => {
+    const {text, isCheck, _id} = el;
 
-  sortAllTasks
-    .forEach(el => {
-      const {text, isCheck, _id} = el;
+    const block = document.createElement("div");
+    const textInTask = document.createElement("div");
+    const buttons = document.createElement("div");
+    const check = document.createElement("input");
+    const deleteBtn = document.createElement("button");
+    const edit = document.createElement("button");
+    block.id = `block-${_id}`;
+    buttons.id = `buttons-${_id}`;
 
-      const block = document.createElement("div");
-      const textInTask = document.createElement("div");
-      const buttons = document.createElement("div");
-      const check = document.createElement("input");
-      const deleteBtn = document.createElement("button");
-      const edit = document.createElement("button");
-      block.id = `block-${_id}`;
-      buttons.id = `buttons-${_id}`;
+    buttons.classList.add("todo__buttons");
 
-      buttons.classList.add("todo__buttons");
+    check.type = "checkbox";
+    check.checked = isCheck;
+    check.classList.add("todo__complete");
 
-      check.type = "checkbox";
-      check.checked = isCheck;
-      check.classList.add("todo__complete");
+    edit.classList.add("todo__changed");
+    edit.innerText = "Редактировать";
 
-      edit.classList.add("todo__changed");
-      edit.innerText = "Редактировать";
+    deleteBtn.classList.add("todo__delete");
+    deleteBtn.innerText = "Удалить";
 
-      deleteBtn.classList.add("todo__delete");
-      deleteBtn.innerText = "Удалить";
+    textInTask.classList.add("todo__text");
+    textInTask.innerText = text;
 
-      textInTask.classList.add("todo__text");
-      textInTask.innerText = text;
+    list.appendChild(block);
+    block.appendChild(textInTask);
+    block.appendChild(buttons);
+    buttons.appendChild(check);
+    buttons.appendChild(edit);
+    buttons.appendChild(deleteBtn);
 
-      list.appendChild(block);
-      block.appendChild(textInTask);
-      block.appendChild(buttons);
+    block.className = isCheck ? "todo__item checked" : "todo__item";
+
+    if (isCheck) {
+      buttons.removeChild(edit);
+    } else {
       buttons.appendChild(check);
       buttons.appendChild(edit);
       buttons.appendChild(deleteBtn);
+    }
 
-      block.className = isCheck ? "todo__item checked" : "todo__item";
+    edit.onclick = () => {
+      onChangeValue(_id, text);
+      buttons.removeChild(edit);
+    };
+    
+    /*
+    Использую стрелочные функции, потому что использую их контекст в области видимости, не выходящей за пределы самих функций
+    */
+    check.onclick = () => {
+      onChangeCheckbox(_id, isCheck);
+    };
 
-      if (isCheck) {
-        buttons.removeChild(edit);
-      } else {
-        buttons.appendChild(check);
-        buttons.appendChild(edit);
-        buttons.appendChild(deleteBtn);
-      }
-
-      edit.onclick = () => {
-        onChangeValue(_id, text);
-        buttons.removeChild(edit);
-      };
-      
-      /*
-        Стрелочные функции лучше всего показывают себя там, где нужно привязать this к контексту, а не к самой функции.
-        Если говорить простыми словами, то, помимо упрощенного синтаксиса и возможности неявного возращения со стороны стрелочной функции, 
-        в классическом выражении функции, ключевое слово this привязывается к различным значениям, в зависимости от контекста, в котором оно вызвано. 
-        В стрелочных функциях, ключ this – лексически привязан. Другими словами, this используется из той части кода, в которой содержится стрелочная функция.
-      */
-      check.onclick = () => {
-        onChangeCheckbox(_id, isCheck);
-      };
-
-      deleteBtn.onclick = () => {
-        onDeleteTask(_id);
-      };
-    });
+    deleteBtn.onclick = () => {
+      onDeleteTask(_id);
+    };
+  });
 };
 
 const onChangeValue = (id, text) => {
@@ -152,8 +146,8 @@ const onChangeValue = (id, text) => {
   };
 };
 
-/*Используются PATCH-запросы потому, что PATCH-запросы позволяют изменять отдельные поля в объекте, не меняя те поля, которые мы не передаем, в случае же
-PUT-запроса, если мы не передаем какое-то из полей в очередном запросе, то это поле будет удалено из БД
+/*
+Использую PATCH, потому что меняю всего лишь одно поле, не затрагивая и не меняя другие
 */
 const saveChange = async(inputTask, id) => {
   try {
